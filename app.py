387e6a2fd8abb4a613e6052a445d4cdf077da576
Maria R Medicci <mmedicci@gmail.com>
2023-02-16 00:27:46 +0000

    terminando o login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g, abort
+import sqlite3
 
 app = Flask("_name_")
 
@@ -15,7 +16,28 @@ posts = [
     }
 ]
 
+# USER MOCKS
+USERNAME = "admin"
+PASSWORD = "admin"
+DATABASE = "blog.bd"
+
+@app.before_request
+def pre_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def encerrar_requisicao(exception):
+    g.bd.close()
+
 @app.route("/")
 def exibir_entradas():
     return render_template("exibir_entradas.html", entradas = posts) 
+
+@app.route('/login', methods = ["GET", "POST"])
+def login ():
+    if request.method == "POST":
+        if request.form["username"] == USERNAME and request.form["password"] == PASSwORD:
+            return "dados recebidos"
+    return render_template("login.html")
+
 
